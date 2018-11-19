c3e5c63643f3b61dc43c7f7bef9a6503004f20b5
Don Nguyen <don@aerofiler.com>
2018-11-19 18:00:10 +1100

    Change case

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-// Make a component
+// Make a Component
 const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
